016e825d4ffc23d3c8e323b937b116de2ce48bf5
Guilherme <guilhermef.bcc@gmail.com>
2021-06-25 00:29:17 -0300

    add controllers, services tests

--- src/test/java/com/dio/personapi/utils/PersonUtils.java
+++ src/test/java/com/dio/personapi/utils/PersonUtils.java
@@ -1,5 +1,9 @@
 package com.dio.personapi.utils;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.dio.personapi.dto.request.PersonDTO;
 import com.dio.personapi.entity.Person;
 
@@ -34,4 +38,17 @@ public class PersonUtils {
                 .phones(Collections.singletonList(PhoneUtils.createFakeEntity()))
                 .build();
     }
+    public static String asJsonString(PersonDTO personDTO) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+            objectMapper.registerModules(new JavaTimeModule());
+
+            return objectMapper.writeValueAsString(personDTO);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
